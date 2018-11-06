d53cc67ce4a4a3b98759c44b826bafd1c6023ac4
JoshuaMiller <admin@joshuamiller.info>
2018-11-06 10:21:23 -0500

    publish final

--- bin/Publish.cmd
+++ bin/Publish.cmd
@@ -1,3 +1,3 @@
 if exist "Photon.Communication\bin\Package\" rmdir /Q /S "Photon.Communication\bin\Package"
-dotnet pack "Photon.Communication\Photon.Communication.csproj" -c Release -o "Photon.Communication\bin\Package"
+dotnet pack "Photon.Communication\Photon.Communication.csproj" -c Release -o "bin\Package"
 nuget push "Photon.Communication\bin\Package\*.nupkg" -Source "https://www.nuget.org/api/v2/package" -NonInteractive
