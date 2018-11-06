d1f6b9bfca6a12f76b02da0a9601c8a43c5a7beb
JoshuaMiller <admin@joshuamiller.info>
2018-11-06 10:16:27 -0500

    publish xml

--- bin/Publish.cmd
+++ bin/Publish.cmd
@@ -1,3 +1,3 @@
 if exist "Photon.Communication\bin\Package\" rmdir /Q /S "Photon.Communication\bin\Package"
-nuget pack "Photon.Communication\Photon.Communication.csproj" -OutputDirectory "Photon.Communication\bin\Package" -Build -Prop "Configuration=Release;Platform=AnyCPU"
+dotnet pack "Photon.Communication\Photon.Communication.csproj" -c Release -o "Photon.Communication\bin\Package"
 nuget push "Photon.Communication\bin\Package\*.nupkg" -Source "https://www.nuget.org/api/v2/package" -NonInteractive
